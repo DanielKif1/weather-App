98ec26e66bd41a1d91c9cdabf3e933d7de3f1fb0
Daniel <kifingadaniel@gmail.com>
2022-12-05 17:44:21 +0200

    first react weather App

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../styles/App.css";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
+// import ForecastSummary from "./ForecastSummary";
+import ForecastSummaries from "./ForecastSummaries";
+
+function App({ location, forecasts }) {
+  // const { location, forecasts } = props;
+
+  return (
+    <div className="weather-app">
+      <LocationDetails
+        className="location-details"
+        city={location.city}
+        country={location.country}
+      />
+      <ForecastSummaries forecasts={forecasts} />
+    </div>
+  );
+}
+
+App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.string,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
+
+export default App;
---
+++ src/components/ForecastSummaries.js
@@ -0,0 +1,36 @@
+/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable react/prop-types */
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/ForecastSummaries.css";
+import ForecastSummary from "./ForecastSummary";
+
+/* eslint-disable  no-unused-vars */
+function ForecastSummaries({ forecasts }) {
+  console.log({ forecasts });
+
+  return (
+    <div className="forecast-summaries">
+      {forecasts.map((forecast) => (
+        <ForecastSummary
+          key={forecast.date}
+          date={forecast.date}
+          description={forecast.description}
+          icon={forecast.icon}
+          temperature={forecast.temperature}
+        />
+      ))}
+    </div>
+  );
+}
+ForecastSummaries.propTypes = {
+  date: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  temperature: PropTypes.shape({
+    max: PropTypes.number,
+    min: PropTypes.number,
+  }).isRequired,
+};
+
+export default ForecastSummaries;
